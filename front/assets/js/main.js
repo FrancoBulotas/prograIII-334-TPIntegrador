24ef60d8ad182fe249356670f1d3547e3591eea3
FrancoBulotas <francothiago10@hotmail.com>
2025-06-29 22:00:09 -0300

    header responsive

--- front/assets/js/main.js
+++ front/assets/js/main.js
@@ -4,3 +4,4 @@ window.addEventListener('DOMContentLoaded', async () => {
     const header = await fetch('./pages/components/header.html').then(response => response.text());
     document.getElementById('header').innerHTML = header;
 });
+
