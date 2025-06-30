b5e19c315e31673d7a7543e78bec2ad74c2f68d3
FrancoBulotas <francothiago10@hotmail.com>
2025-06-30 00:17:39 -0300

    listado de productos agregado

--- front/assets/js/index.js
+++ front/assets/js/index.js
@@ -1,11 +1,56 @@
-let botonIngresa = document.getElementById("boton-ingresar").addEventListener('click', () => {
-        const nombreUsuario = document.getElementById('nombre-usuario').value;
-
-        if (nombreUsuario.trim() === '') {
-            alert('Por favor, ingresa tu nombre.');
-        } else {
-            window.location.href = './pages/productos.html'; 
-        }
-    });
 
 
+window.addEventListener('DOMContentLoaded', async () => {
+    const header = await fetch('/front/pages/components/header.html').then(response => response.text());
+    document.getElementById('header').innerHTML = header;
+});
+
+
+// index.html
+// pantalla de inicio
+let botonIngresa = document.getElementById("boton-ingresar")?.addEventListener('click', () => {
+    const nombreUsuario = document.getElementById('nombre-usuario').value;
+
+    if (nombreUsuario.trim() === '') {
+        alert('Por favor, ingresa tu nombre.');
+    } else {
+        localStorage.setItem('nombreUsuario', nombreUsuario);
+        window.location.href = './pages/productos.html'; 
+    }
+});
+
+
+// productos.html
+
+async function cargarProductos() {
+    const response = await fetch('http://localhost:3000/api/productos');
+    const productos = await response.json();
+
+    const listadoProductos = document.getElementById('listado-productos');
+
+    if (listadoProductos) {
+        productos.payload.forEach(producto => {
+            const productoDiv = document.createElement('div');
+            productoDiv.classList.add('producto');
+
+            if(producto.active) {
+                productoDiv.innerHTML = `
+                <div class="producto-item">
+                    <img src="${producto.imagen}" alt="${producto.nombre}" class="producto-imagen">
+                    <h3>${producto.nombre}</h3>
+                    <p class="p-precio">$ ${producto.precio}</p>
+                    <p class="p-descripcion">${producto.descripcion}</p>
+                    <button class="boton-agregar-carrito">Agregar</button>
+                </div>
+            `;
+            }
+            listadoProductos.appendChild(productoDiv);
+        });
+    }
+}
+
+function init() {
+    cargarProductos();
+}
+
+init(); 
--- front/assets/js/main.js
+++
@@ -1,6 +0,0 @@
-
-window.addEventListener('DOMContentLoaded', async () => {
-    const header = await fetch('./pages/components/header.html').then(response => response.text());
-    document.getElementById('header').innerHTML = header;
-});
-
