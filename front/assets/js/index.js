b38dd41d0addb8ffe3bc27b6e6764c336ece2f72
FrancoBulotas <francothiago10@hotmail.com>
2025-07-04 17:52:19 -0300

    cambiando ruta

--- front/assets/js/index.js
+++ front/assets/js/index.js
@@ -1,7 +1,7 @@
 
 
 window.addEventListener('DOMContentLoaded', async () => {
-    const header = await fetch('/front/pages/components/header.html').then(response => response.text());
+    const header = await fetch('./pages/components/header.html').then(response => response.text());
     document.getElementById('header').innerHTML = header;
 });
 
