e1b753da14d47c8e8c5c269163329bf2bb4e2454
FrancoBulotas <francothiago10@hotmail.com>
2025-07-08 14:11:45 -0300

    me estoy volviendo locooo

--- back/server.js
+++
@@ -1,9 +0,0 @@
-
-import app from './index.js'
-import env from './src/api/config/environments.js';
-
-const PORT = env.port;
-
-app.listen(PORT, () => {
-  console.log(`Servidor local corriendo en puerto ${PORT}`);
-});
--- back/src/api/index.js
+++ back/src/api/index.js
@@ -1,16 +1,20 @@
 
 import express from 'express';
 import cors from 'cors';
-import middlewares from './src/api/middlewares/middlewares.js';
-import { productosRouter, viewRouter, categoriasRouter, ventasRouter } from './src/api/routes/index.js';
-import authRouter from './src/api/controllers/auth.js';
-import { __dirname, join } from './src/api/utils/index.js';
+import middlewares from './middlewares/middlewares.js';
+import { productosRouter, viewRouter, categoriasRouter, ventasRouter } from './routes/index.js';
+import authRouter from './controllers/auth.js';
+import { __dirname, join } from './utils/index.js';
+
+// import env from './config/environments.js';
+
+// const PORT = env.port;
 
 const app = express();
 
 app.set('view engine', 'ejs');
-app.set('views', join(__dirname, 'src/views'));
-app.use(express.static(join(__dirname, 'src/public')));
+app.set('views', join(__dirname, '../views'));
+app.use(express.static(join(__dirname, '../public')));
 
 // Middlewares de aplicacion //
 app.use(express.json()); 
@@ -27,5 +31,11 @@ app.use("/api/auth", authRouter);
 
 // rutas de la aplicacion ejs //
 app.use('/dashboard', viewRouter);
+
+
+
+// app.listen(PORT, () => {
+//   console.log(`Servidor local corriendo en puerto ${PORT}`);
+// });
 
 export default app;
